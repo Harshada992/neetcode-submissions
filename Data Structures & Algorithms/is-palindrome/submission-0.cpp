4df0e60fb09a555d9b7509b2fe49c09bae6f275d
harshada_992 <harshadagaikwad992@gmail.com>
2026-05-18 13:26:07 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+    
+    string newStr="";
+    for(char c :s){
+        if(isalnum(c)){
+            newStr +=tolower(c);
+
+        }
+    }
+
+    return newStr==string(newStr.rbegin(),newStr.rend());
+
+
+        
+        
+    }
+};
